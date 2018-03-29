75387accc756361bb1ab525e31d32cdf376d9158
smoeen <34997264+smoeen@users.noreply.github.com>
2018-03-30 01:36:53 +0500

    uploading test suite

---
+++ BMS/JunitTestSuite.java
@@ -0,0 +1,18 @@
+package BMS;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith (Suite.class)
+@Suite.SuiteClasses({
+	
+	AccTest.class,
+	CustomerTest1.class
+	
+	
+})
+
+
+public class JunitTestSuite {
+
+}
